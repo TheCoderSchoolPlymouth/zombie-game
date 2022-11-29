6d537230bfbdab46b3087f0df6885d3c5118e8fd
lazerphazer <tcs.plymouthcoderschool@gmail.com>
2022-11-29 18:00:08 -0500

    11/29/22

---
+++ Assets/HUMASN SKIN MOVEMENT/crafing.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recipe {
+    public string name;
+    public string output;
+    public bool rotatable;
+    public int out;
+    public string [,] req ;
+}
+public class crafing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
